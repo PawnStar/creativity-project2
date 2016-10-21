d7b5104e7bec7a44c185513226e09389793b07f6
Cole Erickson <pawnstar4@gmail.com>
2016-10-20 23:37:28 -0600

    Added basic text fillout.  Need text classes for each page.

---
+++ text.js
@@ -0,0 +1,26 @@
+messages = {
+  p:[
+    'Police are Prejudiced',
+    'We\'re all prejudiced.'
+  ],
+  o:[
+    'Police are Overstressed',
+    'They\'re quite stressed, so they make poor decisions sometimes.'
+  ],
+  l:[
+    'Police are Lethal',
+    'Unlike the poor decisions of you and me, their poor decisions come at the cost of innocent lives.'
+  ],
+  i:[
+    'Police are Incompetent',
+    'PEPPER SPRAY COP!'
+  ],
+  c:[
+    'Police are Careless',
+    'Sometimes using things not rated for crowd suppression to suppress crowds . . . duh.'
+  ],
+  e:[
+    'Police are Extreme',
+    'Sometimes policing attracts the worst sort of people for the job.'
+  ]
+}
