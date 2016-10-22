9daa80c0e920a50dcbc3b46ce45ef9c227883b3a
Cole Erickson <pawnstar4@gmail.com>
2016-10-21 20:52:13 -0600

    Added clickguards and overlays

--- text.js
+++ text.js
@@ -1,27 +1,32 @@
 messages = {
   p:[
     'Police are Prejudiced',
-    'We\'re all prejudiced.',
-    'And this is the second paragraph.'
+    'Even well meaning officers have unconscious prejudice.  This doesn\'t come because they are bad people or because they intend it to be that way, but because they are subject to the prejudices of their society.',
+    'All people also have biological dispositions to favor those who are like them.  With a large amount of training people can learn to be aware of this and somewhat overcome it, but most officers have had no such training.'
   ],
   o:[
     'Police are Overstressed',
-    'They\'re quite stressed, so they make poor decisions sometimes.'
+    'Police officers are like any other employees: they have quotas, they have deadlines, and they have stress.  This stress affects their ability to make important decisions quickly, which can be dangerous for themselves and those around them.',
+    'Of course everyone has these stressors, but because of the role of police officers in society their stress is much more influential and dangerous to those around them.'
   ],
   l:[
     'Police are Lethal',
-    'Unlike the poor decisions of you and me, their poor decisions come at the cost of innocent lives.'
+    'All people make mistakes, but the unique position police officers have in society means their mistakes have the possibility of bringing permanent, lethal, consequences to those around them.',
+    'Police officers need oversight and good training to ensure they do not make mistakes that are costly to everyone around them.'
   ],
   i:[
     'Police are Incompetent',
-    'PEPPER SPRAY COP!'
+    'Police officers are often sent out into the field with equipment that they don\'t completely understand.',
+    'In November of 2011 this officer pepper sprayed several peaceful protesters directly in the face, not knowing that this would fill their next few days and weeks with a living hell of pain and leave them with lasting injury.'
   ],
   c:[
-    'Police are Careless',
-    'Sometimes using things not rated for crowd suppression to suppress crowds . . . duh.'
+    'Police Departments are Careless',
+    'Aside from the mistakes that single officers make, often their deparments are responsible for that incompetence via careless training.',
+    'Officers need to understand that "non-lethal" weapons are not what they claim to be.  In a 2016 study it was found that almost fifty people out of two thousand die from "non-lethal" methods such as rubber bullets and tear gas.'
   ],
   e:[
     'Police are Extreme',
-    'Sometimes policing attracts the worst sort of people for the job.'
+    'While many police officers are outstanding citizens, the very nature of police work often attracts the worst people for the job.',
+    'Violent and short-tempered individuals not only destroy the good name of the police in general, but they also present a serious danger to the citizens they are to protect.'
   ]
 }
